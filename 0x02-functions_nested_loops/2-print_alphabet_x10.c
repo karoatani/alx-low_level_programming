cee61e107c6248f90bb193eb972e4f48c4ad2bd7
karoatani <karoatani@gmail.com>
2022-06-08 13:21:09 -0700

    adde command

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (i = 97; b <= 122; i++)
+			_putchar(b);
+	}
+	_putchar('\n');
+}
