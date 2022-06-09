e0ea53a17b7cf82b2904c82a2782ba2e866ebdce
karoatani <karoatani@gmail.com>
2022-06-09 15:00:33 -0700

    added fixes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,8 @@ void print_most_numbers(void)
 
 	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch == 50 && ch == 52)
-			continue;
-		_putchar(ch);
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
