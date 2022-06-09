cc604c4d9ec75d3ad373bf5c1dadce4b616512fe
karoatani <karoatani@gmail.com>
2022-06-09 13:34:23 -0700

    added command

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - print number 0-9
+  *
+  * Return: no return(void)
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
