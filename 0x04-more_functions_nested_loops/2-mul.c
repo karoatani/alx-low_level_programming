1dfd6d416a003fd62d7acdb73a591b0c67bb9dc4
karoatani <karoatani@gmail.com>
2022-06-09 13:26:42 -0700

    added command

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul - multiply two numbers
+  *
+  * @a : first parameter
+  * @b : second parameter
+  *
+  * Return: return their multiplication
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
