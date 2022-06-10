fe3bae8c3d65a24b9dd4953bde14b6adf4bd3010
karoatani <karoatani@gmail.com>
2022-06-10 02:37:21 -0700

    added command

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function
+ * @n: parameter
+ * Return: no return.
+ */
+ 
+ void print_diagonal(int n)
+ {
+ 	
+	int i = 1;
+
+	while (i <= n)
+	{
+		_putchar(92);
+		i++;
+	}
+	_putchar('\n');
+ }
