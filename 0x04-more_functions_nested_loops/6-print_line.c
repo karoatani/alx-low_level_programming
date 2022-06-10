4ae3c540b89c70440533dfb0bec28f3020799f3f
karoatani <karoatani@gmail.com>
2022-06-10 02:14:19 -0700

    added command

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *
+ * @n : parameter
+ * Return: no return.
+ */
+
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i <= 5)
+	{
+		putchar(95);
+		i++;
+	}
+}
