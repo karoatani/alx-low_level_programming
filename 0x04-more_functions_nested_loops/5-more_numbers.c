59bff5001f1610c0173b1bf7d2e359f979f41b63
karoatani <karoatani@gmail.com>
2022-06-10 01:40:10 -0700

    added command

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48)
+			_putchar((j % 10) + 48);
+		}
+	}
+}
