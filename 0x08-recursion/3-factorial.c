d244a997c7640b8cbb07524fa166e4d5df5db2cd
karo <karo@karo>
2022-06-22 10:28:19 -0400

    added command

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,20 +1,16 @@
 #include "main.h"
+
 /**
- * factorial - find factorial
- *
- * @n: parameter s
- *
- * Return: factorial
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number.
  */
 int factorial(int n)
 {
-    if ( n < 0)
-        return -1;
-
-    else if (n == 1)
-        return 1;
-    
-    else
-      return  n * factorial(n-1);
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
-
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: base.
+ * @y: exponent.
+ * Return: value of the exponentiation.
+ */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return -1;
-    else if (y ==0){
-        return 1;
-    }
-    else{
-        return x * _pow_recursion(x , y-1);
-    }
-}
-
-int main(void)
-{
-    int r;
-
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
-
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
