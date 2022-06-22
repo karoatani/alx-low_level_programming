0e3813754404df9aa579344d3723d2776bd25136
karo <karo@karo>
2022-06-22 09:55:26 -0400

    added command

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,9 @@
  */
 void _puts_recursion(char *s)
 {
-	static int count;
-
-	count = 0;
-
-	if (*(s + count) != '\0')
+	if (*s != '\0')
 	{
-		_putchar(*(s + count));
-		count++;
+		_putchar(*s);
 		_puts_recursion(s);
 	}
 	else
