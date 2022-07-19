f4b97f69b4f5e2be2e06b835eca7f4d39c197ff2
karo <karo@karo>
2022-07-19 07:46:43 -0400

    added

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *temp2 = *head;
+	listint_t *temp2;
 
-	while ((temp = temp2) != NULL)
+	if (head != NULL)
 	{
-		temp2 = temp2->next;
-		free(temp);
+		temp2 = *head;
+		while ((temp = temp2) != NULL)
+		{
+			temp2 = temp2->next;
+			free(temp);
+		}
 	}
 }
