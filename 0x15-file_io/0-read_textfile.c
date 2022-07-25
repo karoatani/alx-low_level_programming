7cb91e4667fdbbd241cb09954a82400901115196
karo <karo@karo>
2022-07-25 14:55:35 -0400

    committed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,15 +8,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	int sz, rd;
+	int sz, rd, fd;
+	char *c;
 
-	char *c = malloc(sizeof(char) * letters);
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+		return (0);
+	c = malloc(sizeof(char) * letters);
+	if (!c)
 		return (0);
 	rd = read(fd, c, letters);
 	if (rd == -1)
